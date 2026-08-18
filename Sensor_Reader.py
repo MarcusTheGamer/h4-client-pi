948b81ec3c606a75c04abe948540fde5a8a25b39
MarcusTheGamer <splitscreen@cyberbyte.zip>
2026-08-18 11:27:33 +0200

    Commented debug code and removed acknowledgement from MQTT

--- Sensor_Reader.py
+++ Sensor_Reader.py
@@ -17,16 +17,15 @@ def get_hardware_id():
     return "unknown"
 
 HARDWARE_ID = get_hardware_id()
-print(HARDWARE_ID)
+#print(HARDWARE_ID)
 MQTT_BROKER = "192.168.1.138"
 MQTT_PORT = 1883
 API_URL = "http://192.168.1.138:3000/api/sensor-data"
 
 CONFIG_TOPIC = "devices/" + HARDWARE_ID + "/config"
-ACK_TOPIC = "devices/" + HARDWARE_ID + "/ack"
 
 current_config = {}
-SENSORS = {'temperature': {'min': None, 'max': None, 'interval': 60}, 'light': {'min': None, 'max': None, 'interval': 130}, 'humidity': {'min': None, 'max': None, 'interval': 100}, 'sound': {'min': None, 'max': None, 'interval': 120}}
+SENSORS = {'temperature': {'min': 23.0, 'max': 26.0, 'interval': 900}, 'light': {'min': 10.0, 'max': 200.0, 'interval': 900}, 'humidity': {'min': 45.0, 'max': 55.0, 'interval': 1800}, 'sound': {'min': None, 'max': None, 'interval': 120}}
 last_sent = {sensor_type: 0 for sensor_type in SENSORS}      # {sensor_type: last_unix_time_sent}
 alert_state = {}      # {sensor_type: "above" | "below" | "normal"}
 
@@ -45,25 +44,25 @@ def set_config(config):
         for sensor_type in SENSORS:
             if sensor_type not in last_sent:
                 last_sent[sensor_type] = 0
-        print("Config applied:", SENSORS)
+        #print("Config applied:", SENSORS)
     except KeyError as e:
-        print("Bad config, missing key:", e)
+        #print("Bad config, missing key:", e)
 
 def on_connect(client, userdata, flags, rc):
     global mqtt_connected
     if rc == 0:
         mqtt_connected = True
-        print("Connected to MQTT broker as", HARDWARE_ID)
-        client.subscribe([(CONFIG_TOPIC, 0), (ACK_TOPIC, 0)])
-        print("Subscribed to config and ack topics")
+        #print("Connected to MQTT broker as", HARDWARE_ID)
+        client.subscribe([(CONFIG_TOPIC, 0)])
+        #print("Subscribed to config")
     else:
         mqtt_connected = False
-        print("MQTT connect failed, rc =", rc)
+        #print("MQTT connect failed, rc =", rc)
 
 def on_disconnect(client, userdata, rc):
     global mqtt_connected
     mqtt_connected = False
-    print("MQTT connection closed")
+    #print("MQTT connection closed")
 
 def on_message(client, userdata, msg):
     global current_config
@@ -74,9 +73,7 @@ def on_message(client, userdata, msg):
             current_config = json.loads(msg.payload.decode())
             set_config(current_config)
         except json.JSONDecodeError as e:
-            print("Bad config payload:", e)
-    elif msg.topic == ACK_TOPIC:
-        print("Ack:", msg.payload.decode())
+            #print("Bad config payload:", e)
 
 mqtt_client = mqtt.Client(client_id=HARDWARE_ID)
 mqtt_client.on_connect = on_connect
@@ -97,12 +94,12 @@ def post_readings(readings):
     try:
         response = requests.post(API_URL, json=payload, headers=headers, timeout=5)
         response.raise_for_status()
-        print("Posted:", response.json())
+        #print("Posted:", response.json())
         for reading in readings:
-            print(reading)
+            #print(reading)
         return True
     except requests.exceptions.RequestException as e:
-        print("Error:", e)
+        #print("Error:", e)
         return False
 
 def read_sensor(sensor_type):
@@ -196,4 +193,4 @@ while True:
         time.sleep(1)
 
     except (IOError, TypeError) as e:
-        print("Error", e)
+        #print("Error", e)
