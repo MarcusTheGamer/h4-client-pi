5e4aa49b702e18135bc24c988e25f030a5e698b5
Marcus Liam Nielsen <splitscreen@cyberbyte.zip>
2026-08-18 09:00:35 +0200

    Updated set_config

--- Sensor_Reader.py
+++ Sensor_Reader.py
@@ -31,17 +31,12 @@ last_sent = {}
 
 def set_config(config):
     global SENSORS, last_sent
-    print("Raw config:", config)
-    print("Type of config:", type(config))
-    try:
-        SENSORS = config["sensors"]
-        print("SENSORS:", SENSORS)
-        for sensor in SENSORS:
-            print("sensor item:", sensor, type(sensor))
-            if sensor["type"] not in last_sent:
-                last_sent[sensor["type"]] = 0
-    except KeyError as e:
-        print("Bad config, missing key:", e)
+    SENSORS = config["sensors"]
+    if isinstance(SENSORS, str):
+        SENSORS = json.loads(SENSORS)
+    for sensor in SENSORS:
+        if sensor["type"] not in last_sent:
+            last_sent[sensor["type"]] = 0
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
