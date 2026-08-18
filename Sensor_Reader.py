024827dae248f654bc505ac23affc1d7aeba7696
Marcus Liam Nielsen <splitscreen@cyberbyte.zip>
2026-08-18 14:01:15 +0200

    Update Sensor_Reader.py

--- Sensor_Reader.py
+++ Sensor_Reader.py
@@ -5,9 +5,9 @@ from grovepi import *
 import json
 import paho.mqtt.client as mqtt
 
-th_port = 7 # temperature and humidity port 7
-light_port = 0 # light port 0
-sound_port = 1 # sound port 1
+th_port = 7
+light_port = 0
+sound_port = 1
 
 def get_hardware_id():
     with open('/proc/cpuinfo', 'r') as f:
@@ -17,7 +17,6 @@ def get_hardware_id():
     return "unknown"
 
 HARDWARE_ID = get_hardware_id()
-#print(HARDWARE_ID)
 MQTT_BROKER = "192.168.1.138"
 MQTT_PORT = 1883
 API_URL = "http://192.168.1.138:3000/api/sensor-data"
@@ -26,12 +25,12 @@ CONFIG_TOPIC = "devices/" + HARDWARE_ID + "/config"
 
 current_config = {}
 SENSORS = {'temperature': {'min': 23.0, 'max': 26.0, 'interval': 900}, 'light': {'min': 10.0, 'max': 200.0, 'interval': 900}, 'humidity': {'min': 45.0, 'max': 55.0, 'interval': 1800}, 'sound': {'min': None, 'max': None, 'interval': 120}}
-last_sent = {sensor_type: 0 for sensor_type in SENSORS}      # {sensor_type: last_unix_time_sent}
-alert_state = {}      # {sensor_type: "above" | "below" | "normal"}
+last_sent = {sensor_type: 0 for sensor_type in SENSORS}
+alert_state = {}
 
 mqtt_connected = False
 last_post_ok = True
-last_readings_text = "HARDWARE ID: \n" + HARDWARE_ID
+display_text = "HARDWARE ID: \n" + HARDWARE_ID
 display_toggle = 0
 
 READINGS_DURATION = 3
@@ -83,10 +82,8 @@ def post_readings(readings):
     try:
         response = requests.post(API_URL, json=payload, headers=headers, timeout=5)
         response.raise_for_status()
-        #print("Posted:", response.json())
         return True
     except requests.exceptions.RequestException as e:
-        #print("Error:", e)
         return False
 
 def read_sensor(sensor_type):
@@ -124,15 +121,7 @@ while True:
     values = {}
     posted_this_tick = False
 
-    sensors_to_read = set()
-    for sensor_type, settings in SENSORS.items():
-        interval = settings.get("interval")
-        due = interval is not None and now - last_sent[sensor_type] >= interval
-        monitored = settings.get("min") is not None or settings.get("max") is not None
-        if due or monitored:
-            sensors_to_read.add(sensor_type)
-
-    for sensor_type in sensors_to_read:
+    for sensor_type in SENSORS:
         if sensor_type not in values:
             values.update(read_sensor(sensor_type))
 
@@ -147,7 +136,6 @@ while True:
             if check_threshold(sensor_type, value, settings):
                 alert_readings.append({"sensor_type": sensor_type, "value": value})
 
-        # Interval schedule is untouched by alert posts above
         if interval is not None and now - last_sent[sensor_type] >= interval:
             readings.append({"sensor_type": sensor_type, "value": value})
             last_sent[sensor_type] = now
@@ -159,15 +147,15 @@ while True:
             posted_this_tick = True
 
     if readings:
-        text = ""
-        for r in readings:
-            text += str(r["sensor_type"]).split()[0][:2] + "=" + str(r["value"]) + ","
-        last_readings_text = text
-
         ok = post_readings(readings)
         last_post_ok = ok
         if ok:
             posted_this_tick = True
+
+    values_text = ""
+    for sensor_type in SENSORS:
+        if sensor_type in values:
+            values_text += str(sensor_type).split()[0][:2] + "=" + str(values[sensor_type]) + ","
 
     error_state = (not mqtt_connected) or (not last_post_ok)
     alert_active = any(state != "normal" for state in alert_state.values())
@@ -184,17 +172,19 @@ while True:
         display_toggle += 1
         cycle_position = display_toggle % (READINGS_DURATION + ID_DURATION)
         if cycle_position < READINGS_DURATION:
-            setText(last_readings_text)
+            display_text = values_text
         else:
-            setText("HARDWARE ID: \n" + HARDWARE_ID)
+            display_text = "HARDWARE ID: \n" + HARDWARE_ID
     elif posted_this_tick:
         setRGB(0, 255, 0)
-        setText(last_readings_text)
+        display_text = values_text
     elif alert_active:
         setRGB(255, 255, 0)
-        setText(last_readings_text)
+        display_text = values_text
     else:
         setRGB(0, 255, 0)
-        setText(last_readings_text)
+        display_text = values_text
+
+    setText(display_text)
 
     time.sleep(1)
